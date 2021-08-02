509e335f1976f38c46af2ab664a101b7379fb977
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-02 16:20:39 +0900

    fizz buzz

--- fizz_buzz/fizz_buzz.rs
+++ fizz_buzz/fizz_buzz.rs
@@ -11,20 +11,34 @@
 //         }
 //     }
 // }
-//
-//
 
+// fn main () {
+//     for n in 1..=100 {
+//         if n % 15 == 0 {
+//             println!("fizzbuzz");
+//         } else if n % 5 == 0 {
+//             println!("buzz");
+//         } else if n % 3 == 0 {
+//             println!("fizz");
+//         } else {
+//             println!("{}", n);
+//         }
+//     }
+// }
 
-fn main () {
-    for n in 1..=100 {
-        if n % 15 == 0 {
+fn main() {
+    for n in 1..100 {
+        let can_div_by_five = n % 5 == 0;
+        let can_div_by_three = n % 3 == 0;
+
+        if can_div_by_five && can_div_by_three {
             println!("fizzbuzz");
-        } else if n % 5 == 0 {
+        } else if can_div_by_five {
             println!("buzz");
-        } else if n % 3 == 0 {
+        } else if can_div_by_three {
             println!("fizz");
         } else {
-            println!("{}", n);
+            println!("{}", n)
         }
     }
 }
