1c0f17ffa2ab199ac1ac2c7f5bac04f0e769569d
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-02 20:04:05 +0900

    done rock_paper_scissors game

--- rock_paper_scissors/src/main.rs
+++ rock_paper_scissors/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let lose = "lose";
     let draw = "draw";
 
+    // enum is better
     let options = ["rock", "paper", "scissors"];
 
     loop {
@@ -16,18 +17,37 @@ fn main() {
         let mut computer_hand = pick_random(&options);
 
         println!("enter your hand plz: Type rock, paper, scissors");
+
         // handle standard input
         // read_line is one of function of str::io
         // return [&mut String]
         // expect is panic func
         io::stdin().read_line(&mut player_input).expect("failed");
 
+        // remove white space
         let player_hand = trim_space(&player_input);
+
         // check player_hand is valid or not
         if player_hand == "rock" || player_hand == "paper" || player_hand == "scissors" {
             println!("your hand: {}", player_input);
             println!("computer hand: {}", computer_hand);
-            continue;
+            // player vs computer
+            // more smarter func in future....
+            if player_hand == computer_hand {
+                println!("{}", draw);
+            } else if player_hand == "rock" || computer_hand == "paper" {
+                println!("You are: {}", lose);
+            } else if player_hand == "rock" || computer_hand == "scissors" {
+                println!("You are: {}", win);
+            } else if player_hand == "paper" || computer_hand == "scissors" {
+                println!("You are: {}", lose);
+            } else if player_hand == "paper" || computer_hand == "rock" {
+                println!("You are: {}", win);
+            } else if player_hand == "scissors" || computer_hand == "rock" {
+                println!("You are: {}", lose);
+            } else {
+                println!("You are: {}", win);
+            }
         } else {
             println!("invalid input");
             break;
@@ -36,7 +56,7 @@ fn main() {
 }
 
 // remove white space
-fn trim_space (input: &String)-> String {
+fn trim_space(input: &String) -> String {
     return input.trim().to_string();
 }
 
