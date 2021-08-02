09f903be4609225abad27221a920f061bfcfc3c0
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-02 17:31:40 +0900

    change rand version

--- rock_paper_scissors/src/main.rs
+++ rock_paper_scissors/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::{Rng, thread_rng};
 
 fn main() {
 
@@ -13,12 +14,30 @@ fn main() {
         // player_hand
         let mut player_input = String::new();
 
+        // computer hand is generated random
+        let mut computer_hand = pick_random(&options);
+
+        println!("enter your hand plz: Type rock, paper, scissors");
+
+        if player_input == "rock" || player_input == "paper" || player_input == "scissors" {
+            println!("invalid input");
+            break;
+        }
+
         // handle standard input
         // read_line is one of function of str::io
         // return [&mut String]
         // expect is panic func
         io::stdin().read_line(&mut player_input).expect("failed");
 
-        println!("{}", player_input);
+        println!("your hand: {}", player_input);
+        println!("computer hand: {}", computer_hand);
     }
+}
+
+fn pick_random(options: &[&str]) -> String {
+    // Returns a random member from an array of strs
+    let mut rng = thread_rng();
+    let index = rng.gen_range(0..options.len());
+    return String::from(options[index]);
 }
