a6d9e2c4ef2bad130e9cbff2e34b207dee9218ab
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-03 13:50:37 +0900

    done guess number

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,31 +1,33 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let mut input_number = String::new();
-
     // create random number
     let random_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut input_number = String::new();
 
-    println!("Guess number game");
-    println!("input your guess");
+        println!("Guess number game");
+        println!("input your guess");
 
-    // get player number
-    io::stdin().read_line(&mut input_number).expect("error");
+        // get player number
+        io::stdin().read_line(&mut input_number).expect("error");
 
-    // input_number convert string to number (u32)
-    // same variable name = shadowing
-    let input_number: u32 = input_number.trim().parse().expect("plz input number");
+        // input_number convert string to number (u32)
+        // same variable name = shadowing
+        let input_number: u32 = input_number.trim().parse().expect("plz input number");
 
-    // check player number is collect or not
-                  match input_number.cmp(&random_number) {
-                    Ordering::Greater => println!("TOO BIG!!!"),
-                    Ordering::Less => println!("too small..."),
-                    Ordering::Equal => println!("collect!!!"),
-                  }
+        println!("Your guess is: {}", input_number);
 
-    println!("{}", random_number);
-    println!("Your guess is: {}", input_number);
+        // check player number is collect or not
+        match input_number.cmp(&random_number) {
+            Ordering::Greater => println!("TOO BIG!!!"),
+            Ordering::Less => println!("too small..."),
+            Ordering::Equal => {
+                println!("collect!!!");
+                break;
+            }
+        }
+    }
 }
-
