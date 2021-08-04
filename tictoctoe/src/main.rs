77da6c8f6e107bda3162e43251a4f4650f113ef1
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-04 17:38:13 +0900

    init tictoctoe

---
+++ tictoctoe/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // state definition ( Board )
+
+    // Player definition
+    // x => you
+    // o => computer
+    pub enum Player {
+        x,
+        o,
+    }
+
+    // score definition
+
+    // move func
+
+    // check func  (is win, lose)
+}
