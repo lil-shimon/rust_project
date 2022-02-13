09244aa3a0bd087061ab7400b7d69206ae60f590
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-13 15:31:31 +0900

    feat: 🎸 ownership

--- test/rust-practice/src/ownership.rs
+++ test/rust-practice/src/ownership.rs
@@ -1,7 +1,9 @@
 pub fn run() {
     let s1 = String::from("hello"); // s1が所有権を持っている
-    // s1からs2に所有権を渡している
-    let s2 = s1;
-    println!("{}", s2)
-    // s1は所有権がないからエラー (value borrowed here after move)
+    let s2 = s1; // s1からs2に所有権を渡している
+    println!("{}", s2); // s1は所有権がないからエラー (value borrowed here after move)
+
+    let i1 = 1;// i1が所有権を持っている
+    let i2 = i1; // i2にコピー (heapにデータを保存しないタイプはコピーできる)
+    println!("{}, {}", i1, i2); // i1とi2が表示される
 }
