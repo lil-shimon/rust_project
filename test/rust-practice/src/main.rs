116cc180137bd28b542d8117220055d2c98aadf1
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-16 23:23:41 +0900

    feat: 🎸 error

--- test/rust-practice/src/main.rs
+++ test/rust-practice/src/main.rs
@@ -12,7 +12,19 @@ fn main() {
     let apple = Apple {};
     let banana = Banana {};
     get_price(apple);
-    get_price(banana)
+    get_price(banana);
+
+    let res = div_opt(4.0, 2.0);
+    match res {
+        Some(x) => println!("Result: {:.3}", x),
+        None => println!("Not allowed!")
+    }
+
+    let res_1 = div_result(20.0, 3.9);
+    match res_1 {
+        Ok(x) => println!("Result: {:.3}", x),
+        Err(e) => println!("{}", e)
+    }
 }
 
 trait Fruits {
@@ -37,4 +49,20 @@ impl Fruits for Banana {
 
 fn get_price<T: Fruits>(fruits: T) {
     println!("price is: {}", fruits.price())
+}
+
+fn div_opt(x: f64, y: f64) -> Option<f64> {
+    if y == 0.0 {
+        None
+    } else {
+        Some(x / y)
+    }
+}
+
+fn div_result(x: f64, y: f64) -> Result<f64, String> {
+    if y == 0.0 {
+        Err(String::from("Not allowed"))
+    } else {
+        Ok(x / y)
+    }
 }
